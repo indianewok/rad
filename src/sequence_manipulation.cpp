0c7bc3471517e5dad4618c0218eb339c87b98bc1
Chinmay M. Vaidya <chinmay.m.vaidya.GR@dartmouth.edu>
2024-02-02 18:32:21 -0500

    First repo push!

---
+++ src/sequence_manipulation.cpp
@@ -0,0 +1,103 @@
+#include <Rcpp.h>
+#include <vector>
+#include <string>
+#include <cstdint>
+#include </dartfs-hpc/rc/home/3/f005f43/jaeger/src/RcppInt64.h>
+
+using namespace std;
+using namespace Rcpp;
+
+std::string revcomp_cpp(const std::string& sequence) {
+  std::unordered_map<char, char> complement {
+    {'A', 'T'}, {'T', 'A'}, {'C', 'G'}, {'G', 'C'},
+    {'a', 't'}, {'t', 'a'}, {'c', 'g'}, {'g', 'c'},
+    {'N', 'N'}, {'n', 'n'}
+  };
+  std::string reversed(sequence.rbegin(), sequence.rend());
+  for (char& nucleotide : reversed) {
+    nucleotide = complement[nucleotide];
+  }
+  return reversed;
+}
+
+// [[Rcpp::export]]
+Rcpp::StringVector revcomp(Rcpp::StringVector sequences) {
+  int n = sequences.size();
+  Rcpp::StringVector results(n);
+  int j = 0; // Counter for results
+  for (int i = 0; i < n; ++i) {
+    if (sequences[i] == NA_STRING) {
+      results[j] = NA_STRING;
+      continue;
+    }
+    results[j] = revcomp_cpp(Rcpp::as<std::string>(sequences[i]));
+    ++j;
+  }
+  return results;
+}
+
+std::vector<int64_t> sequence_to_bits_cpp(const std::string& sequence) {
+  const int chunk_size = 32; // Number of bases per chunk
+  std::vector<int64_t> results;
+  for (size_t i = 0; i < sequence.size(); i += chunk_size) {
+    int64_t result = 0;
+    size_t end = std::min(i + chunk_size, sequence.size());
+    for (size_t j = i; j < end; ++j) {
+      char c = sequence[j];
+      result <<= 2;
+      switch (c) {
+      case 'A': break;
+      case 'C': result |= 1; break;
+      case 'T': result |= 2; break;
+      case 'G': result |= 3; break;
+      }
+    }
+    results.push_back(result);
+  }
+  return results;
+}
+
+// [[Rcpp::export]]
+Rcpp::NumericVector sequence_to_bits(Rcpp::StringVector sequences) {
+  int n = sequences.size();
+  std::vector<int64_t> results;
+  for(int i = 0; i < n; ++i) {
+    std::string sequence = Rcpp::as<std::string>(sequences[i]);
+    std::vector<int64_t> chunk_results = sequence_to_bits_cpp(sequence);
+    results.insert(results.end(), chunk_results.begin(), chunk_results.end());
+  }
+  return Rcpp::toInteger64(results);
+}
+
+std::string bits_to_sequence_cpp(const std::vector<int64_t>& input, int sequence_length) {
+  int n = input.size();
+  std::string final_sequence;
+  int chunk_size = 32;
+
+  for (int i = 0; i < n; ++i) {
+    int64_t int64_code = input[i];
+    std::string result;
+
+    int bases_to_decode = (i < n - 1) ? chunk_size : sequence_length % chunk_size;
+    bases_to_decode = (bases_to_decode == 0) ? chunk_size : bases_to_decode;
+
+    for (int j = 0; j < bases_to_decode; ++j) {
+      switch (int64_code & 3) {
+      case 0: result.insert(result.begin(), 'A'); break;
+      case 1: result.insert(result.begin(), 'C'); break;
+      case 2: result.insert(result.begin(), 'T'); break;
+      case 3: result.insert(result.begin(), 'G'); break;
+      }
+      int64_code >>= 2;
+    }
+    final_sequence += result;
+  }
+  return final_sequence;
+}
+
+// [[Rcpp::export]]
+Rcpp::StringVector bits_to_sequence(Rcpp::NumericVector input, int sequence_length) {
+  std::vector<int64_t> bits_input = Rcpp::as<std::vector<int64_t>>(input);
+  std::string sequence = bits_to_sequence_cpp(bits_input, sequence_length);
+  return Rcpp::wrap(Rcpp::StringVector::create(sequence));
+}
