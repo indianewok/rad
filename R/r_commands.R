552dcc93ff93ec4a3c11e363bc197dac16e8d72a
CMV <chinmay.m.vaidya.GR@dartmouth.edu>
2024-05-13 11:35:56 -0400

    moving a stop output to actually stop the code, see if this works?

--- R/r_commands.R
+++ R/r_commands.R
@@ -463,8 +463,8 @@ whitelist_generator<-function(df, original_whitelist = NULL, prefiltered_whiteli
       print("Something's a little funky, returning all of the material on hand and stopping execution!")
       troubleshooting_output<-list(barcodes = barcodes, df = df)
       return(list2env(troubleshooting_output))
-      stop()
     }
+    stop()
   }
   if(ratio > 0.5){
     rescue_repeats<-6
